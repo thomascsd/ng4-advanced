bc838e7ab46854c639e5a1c15f601193df2ce08c
thomascsd <thomascsd808@gmail.com>
2017-10-29 11:33:11 +0800

    06 套用 WB0901P31-HTML5 網頁版型並設定 dashboard 與 cards 的路由設定

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { Page1Component } from './page1/page1.component';
-import { Page2Component } from './page2/page2.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CardsComponent } from './cards/cards.component';
+
 
 const routes: Routes = [
-  { path: '', redirectTo: 'page1', pathMatch: 'full' },
-  { path: 'page1', component: Page1Component },
-  { path: 'page2', component: Page2Component }
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'cards', component: CardsComponent }
+
 ];
 
 @NgModule({
