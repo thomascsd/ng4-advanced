06d0116f44dad4e0aea41879960f70d1cc5a24be
thomascsd <thomascsd808@gmail.com>
2017-11-05 09:44:51 +0800

    16 建立 form1 與 form2 頁面並設定正確路由顯示表單頁面

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,22 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CardsComponent } from './cards/cards.component';
 import { LayoutComponent } from './layout/layout.component';
 import { LoginComponent } from './login/login.component';
+import { Form1Component } from './form1/form1.component';
+import { Form2Component } from './form2/form2.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '', redirectTo: 'form1', pathMatch: 'full' },
   {
     path: '', component: LayoutComponent,
     children: [
       { path: 'dashboard', component: DashboardComponent },
       { path: 'cards/:type', component: CardsComponent },
-      { path: 'charts', loadChildren: './charts/charts.module#ChartsModule' }
+      { path: 'charts', loadChildren: './charts/charts.module#ChartsModule' },
+      { path: 'form1', component: Form1Component },
+      { path: 'form2', component: Form2Component },
     ]
   },
   { path: 'login', component: LoginComponent }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CardsComponent } from './cards/cards.component';
 import { LayoutComponent } from './layout/layout.component';
 import { LoginComponent } from './login/login.component';
+import { Form1Component } from './form1/form1.component';
+import { Form2Component } from './form2/form2.component';
 
 @NgModule({
   declarations: [
@@ -13,10 +16,14 @@ import { LoginComponent } from './login/login.component';
     DashboardComponent,
     CardsComponent,
     LayoutComponent,
-    LoginComponent
+    LoginComponent,
+    Form1Component,
+    Form2Component
 ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
