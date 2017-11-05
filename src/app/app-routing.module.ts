4495aca40131d52b2ba68da0c5ef7e5bdbcfa7e4
thomascsd <thomascsd808@gmail.com>
2017-11-05 15:11:45 +0800

    21 練習 FormGroup 與 FormArray 的應用技巧

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { Form1Component } from './form1/form1.component';
 import { Form2Component } from './form2/form2.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'form1', pathMatch: 'full' },
+  { path: '', redirectTo: 'form2', pathMatch: 'full' },
   {
     path: '', component: LayoutComponent,
     children: [
--- src/app/form2/form2.component.ts
+++ src/app/form2/form2.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-form2',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form2.component.css']
 })
 export class Form2Component implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      title: ['', Validators.required],
+      subTitle: ['', [Validators.required, Validators.pattern('\\d+')]],
+      people: this.fb.array([
+        this.addPersonCtrl(),
+        this.addPersonCtrl()
+      ])
+    });
+
+  }
+
+  addPerson() {
+    const people = this.form.get('people') as FormArray;
+    people.push(this.addPersonCtrl());
+  }
+
+  addPersonCtrl(): FormGroup {
+    return this.fb.group({
+      name: '',
+      mobile: '',
+      email: ''
+    });
   }
 
 }
